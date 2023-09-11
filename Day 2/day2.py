c1f2ba7e07437a2479c5711fac4212bd00eb4635
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-09-11 22:33:52 +0530

    Create day2.py

---
+++ Day 2/day2.py
@@ -0,0 +1,27 @@
+class Solution:
+    #Function to return the position of the first repeating element.
+    def firstRepeated(self,arr, n):
+        if len(arr) == len(set(arr)):
+            return -1
+        for i in range(n):
+            if arr.count(arr[i]) > 1:
+                return i + 1
+        return -1
+        
+        #arr : given array
+        #n : size of the array
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        
+        arr=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.firstRepeated(arr, n))
+# } Driver Code Ends
