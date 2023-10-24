f51231a9b3277dd8ebe1bde4a1a81200ea58ab64
Aswin George Thomas <75844342+aswingt65@users.noreply.github.com>
2023-10-24 22:33:31 +0530

    Create day45.py

---
+++ Day 45/day45.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def shortestCommonSupersequence(self, X, Y, m, n):
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if X[i-1] == Y[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    
+        return m + n - dp[m][n]
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__': 
+    t=int(input())
+    for tcs in range(t):
+        X,Y=input().split()
+        
+        print(Solution().shortestCommonSupersequence(X,Y,len(X),len(Y)))
+        
+# } Driver Code Ends
